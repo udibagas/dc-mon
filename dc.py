7774f318a1fef1b513a641edfc8c25034dad8d2c
Bagas Udi Sahsangka <udibagas@gmail.com>
2017-10-16 20:51:43 +0700

    format logger

--- dc.py
+++ dc.py
@@ -153,7 +153,8 @@ if __name__ == "__main__":
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler('dc.log')
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
